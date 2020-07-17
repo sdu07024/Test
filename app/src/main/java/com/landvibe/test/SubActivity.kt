7c01ad2e62e5700fef94dbb0a4b708be49bdbd00
DESKTOP-DPCVCKC\김혜인 <sdu07024@naver.com>
2020-07-17 20:01:49 +0900

    sec_com

---
+++ app/src/main/java/com/landvibe/test/SubActivity.kt
@@ -0,0 +1,15 @@
+package com.landvibe.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sub.*
+
+class SubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+
+        if(intent.hasExtra("msg"))
+            tv_get.text = intent.getStringExtra("msg")
+    }
+}
